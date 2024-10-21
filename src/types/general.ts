a4953ccea8aad247c0e7228c368b9b8828cfb15b
emorozov28 <emorozov328@gmail.com>
2024-10-21 23:20:54 +0300

    Enhance YouTube lazy loading

---
+++ src/types/general.ts
@@ -0,0 +1,11 @@
+export enum BackgroundQuality {
+    HQ_DEFAULT = 'hqdefault',
+    MQ_DEFAULT = 'mqdefault',
+    SD_DEFAULT = 'sddefault',
+    MAX_RES_DEFAULT = 'maxresdefault',
+}
+
+export interface IOptionsConfig {
+    buttonContent?: string; 
+    customBackgroundQuality?: BackgroundQuality;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -7,83 +9,93 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
 
-const config = {
-    entry: './src/index.js',
-    mode: 'production',
-    output: {},
+const getPlugins = (isDev) => {
+    const plugins = [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `index.css`
+        })
+    ];
+
+    if (isDev) {
+        plugins.push(
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, './src/index.html'),
+                filename: 'index.html',
+                minify: false
+            }),
+            new CopyWebpackPlugin({
+                patterns: [{
+                    from: path.resolve(__dirname, 'src/img'),
+                    to: path.resolve(__dirname, 'demo/img')
+                }]
+            })
+        );
+    }
+
+    return plugins;
+};
+
+const baseConfig = {
+    entry: './src/index.ts',
+    mode: isProd ? 'production' : 'development',
     devtool: isProd ? false : 'source-map',
-    plugins: [],
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
     module: {
-        rules: [
+        rules: [{
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: 'babel-loader',
             },
             {
                 test: /\.(s[ac]ss|css)$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'postcss-loader',
-                    'sass-loader'
-                ]
+                    'sass-loader',
+                ],
             },
-        ]
-    }
+        ],
+    },
 };
 
 const configDev = {
-    ...config,
+    ...baseConfig,
     output: {
         library: 'LazyLoadYouTube',
         libraryTarget: 'umd',
         libraryExport: 'default',
         path: path.resolve(__dirname, 'demo'),
-        filename: 'index.js'
+        filename: 'index.js',
     },
     devServer: {
         port: 8080,
         historyApiFallback: true,
         open: true,
         compress: true,
-        hot: true
+        hot: true,
     },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-            minify: false
-        }),
-        new MiniCssExtractPlugin({filename: `index.css`}),
-        new CopyWebpackPlugin({
-            patterns: [
-            {
-                from: path.resolve(__dirname, 'src/img'),
-                to: path.resolve(__dirname, 'demo/img')
-            }
-        ]
-        })
-    ],
-}
+    plugins: getPlugins(true),
+};
 
 const configProd = {
-    ...config,
+    ...baseConfig,
     output: {
         library: 'LazyLoadYouTube',
         libraryTarget: 'umd',
         libraryExport: 'default',
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
+        filename: 'index.js',
     },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({filename: `index.css`}),
-    ],
-}
-
+    plugins: getPlugins(false),
+};
 
 module.exports = [configDev, configProd];
